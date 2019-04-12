782ee2132b3e5c89449da9e80a58a753d1c705fb
maandeep98 <maandeep998@gmail.com>
2019-04-12 15:11:14 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,25 @@
+(function() {
+'use strict';
+
+  angular.module('NameCalculator', [])
+
+  .controller('NameCalculatorController', function($scope) {
+    $scope.name = '';
+    $scope.totalValue = 0;
+    $scope.displayNumeric = function() {
+      var totalValue = Calculate($scope.name);
+      $scope.totalValue = totalValue;
+    };
+
+    function Calculate(string) {
+      var totalStringValue = 0;
+      for(var i = 0; i < string.length; i++) {
+        totalStringValue += string.charCodeAt(i);
+      }
+
+      return totalStringValue;
+    }
+
+  });
+
+})();
